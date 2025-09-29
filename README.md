1e8651efe5aeeda988a63d713ced17ff69674687
keshavsoft <nknnkumar@yahoo.com>
2025-09-29 17:25:55 +0530

    v 1.0.5

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ### 1.0.4
 
 - version started
+
+### 1.0.5
+
+- version 4 with new table BillItemsTable started
---
+++ SV4/routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+import { router as routerFromBillItemsTable } from "./BillItemsTable/routes.js";
+import { router as routerFromUsersTable } from "./UsersTable/routes.js";
+import { router as routerFromTokenTable } from "./TokenTable/routes.js";
+import { router as routerFromItemsTable } from "./ItemsTable/routes.js";
+
+router.use("/BillItemsTable", routerFromBillItemsTable);
+router.use("/UsersTable", routerFromUsersTable);
+router.use("/TokenTable", routerFromTokenTable);
+router.use("/ItemsTable", routerFromItemsTable);
+
+export { router };
